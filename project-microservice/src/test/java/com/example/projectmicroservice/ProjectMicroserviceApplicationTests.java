c60ddde9603f9d64a06d7fe20521d696553b4a79
Marieta Stoycheva <mar.stoycheva@gmail>
2026-03-06 17:44:02 +0200

    Add microservice modules

---
+++ project-microservice/src/test/java/com/example/projectmicroservice/ProjectMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
