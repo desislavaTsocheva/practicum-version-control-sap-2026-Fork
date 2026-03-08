f2ad2afbf90f10fbe1cbb50c1b9cc0b6b8f59531
desislavaTsocheva <desislava_tsocheva@abv.bg>
2026-03-08 22:11:27 +0200

    Create model for users table

--- auth-microservice/src/main/java/com/example/authmicroservice/models/User.java
+++ auth-microservice/src/main/java/com/example/authmicroservice/models/User.java
@@ -1,4 +1,39 @@
 package com.example.authmicroservice.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", length = 50)
+    private String lastName;
+
+    @Column(name = "role", nullable = false, length = 50)
+    private String role;
+
+    @Column(name = "password", nullable = false, length = 50)
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false, length = 255)
+    private String email;
 }
