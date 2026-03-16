0828cac66fb785d49c060b651ce14107785ab800
desislavaTsocheva <desislava_tsocheva@abv.bg>
2026-03-17 00:06:57 +0200

    Create projects logic

--- project-microservice/src/main/java/com/example/projectmicroservice/controllers/ProjectController.java
+++ project-microservice/src/main/java/com/example/projectmicroservice/controllers/ProjectController.java
@@ -1,13 +1,41 @@
 package com.example.projectmicroservice.controllers;
+import com.example.projectmicroservice.models.Project;
+import com.example.projectmicroservice.services.ProjectService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Controller
-@RequestMapping("/projects")
 public class ProjectController {
-    @GetMapping
-    public String getProjects() {
-        return "projects";
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping("/projects")
+    public String newProject(
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam("ownerId") UUID ownerId
+    ) {
+        Project project = new Project();
+        project.setName(name);
+        project.setDescription(description);
+        project.setCreatedAt(LocalDateTime.now());
+
+        if (ownerId == null) {
+            ownerId = UUID.randomUUID();
+        }
+        project.setOwnerId(ownerId);
+
+        projectService.saveProject(project);
+
+        return "redirect:http://localhost:8080/document-microservice/documents?userId="
+                + ownerId + "&name=" + name;
     }
 }
+
