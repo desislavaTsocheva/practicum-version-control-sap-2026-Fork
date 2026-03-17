c59d49fde83ef4df6258f88c21b8b8050dcc3b74
Marieta Stoycheva <mar.stoycheva@gmail>
2026-03-17 13:30:32 +0200

    Fix buttons and profile pictures logic

--- auth-microservice/src/main/java/com/example/authmicroservice/controllers/UserController.java
+++ auth-microservice/src/main/java/com/example/authmicroservice/controllers/UserController.java
@@ -1,14 +1,40 @@
 package com.example.authmicroservice.controllers;
 
+import com.example.authmicroservice.models.User;
+import com.example.authmicroservice.services.UserService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/users/profile-pic/{id}")
+    @ResponseBody
+    public ResponseEntity<byte[]> getProfilePic(@PathVariable UUID id) {
+        User user = userService.findById(id);
+        if (user != null && user.getProfilePicture() != null) {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(user.getProfilePicture());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
