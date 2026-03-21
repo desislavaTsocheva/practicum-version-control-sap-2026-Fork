cf18a7c4d266d26e62dff921a5dee463fc553414
Marieta Stoycheva <mar.stoycheva@gmail>
2026-03-21 14:12:46 +0200

    Fix edit user information logic and hide password

--- auth-microservice/src/main/java/com/example/authmicroservice/controllers/UserController.java
+++ auth-microservice/src/main/java/com/example/authmicroservice/controllers/UserController.java
@@ -7,14 +7,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.security.Principal;
 import java.util.Base64;
+import java.util.Map;
 import java.util.UUID;
 
 @Controller
@@ -34,20 +32,17 @@ public class UserController {
     @GetMapping("/users/profile-pic/{id}")
     @ResponseBody
     public ResponseEntity<byte[]> getProfilePic(@PathVariable UUID id) {
-        System.out.println(">>> Търся снимка за потребител ID: " + id);
         User user = userService.findById(id);
 
         if (user == null) {
-            System.out.println(">>> ГРЕШКА: Потребителят не съществува в базата!");
             return ResponseEntity.notFound().build();
         }
 
         if (user.getProfilePicture() == null) {
-            System.out.println(">>> ГРЕШКА: Потребителят е намерен, но няма записана снимка!");
             return ResponseEntity.notFound().build();
         }
 
-        System.out.println(">>> УСПЕХ: Снимката е намерена, размер: " + user.getProfilePicture().length + " bytes");
+        System.out.println("size of pfp: " + user.getProfilePicture().length + " bytes");
         return ResponseEntity.ok()
                 .contentType(MediaType.IMAGE_PNG)
                 .body(user.getProfilePicture());
@@ -57,5 +52,21 @@ public class UserController {
         User user = userService.findById(id);
         model.addAttribute("user", user);
         return "profile-page";
+    }
+
+    @PostMapping("/users/profile/update/{id}")
+    @ResponseBody
+    public ResponseEntity<?> updateProfile(@PathVariable UUID id, @RequestBody Map<String, String> updates) {
+        User user = userService.findById(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        user.setFirstName(updates.get("firstName"));
+        user.setLastName(updates.get("lastName"));
+        user.setEmail(updates.get("email"));
+
+        userService.save(user);
+        return ResponseEntity.ok().body("{\"message\": \"Success\"}");
     }
 }
