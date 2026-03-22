53871dd6fe568878a16a015ce1e6fd4aa716a9bb
Marieta Stoycheva <mar.stoycheva@gmail>
2026-03-22 21:30:15 +0200

    Work on admin CRUD

--- document-microservice/src/main/java/com/example/documentmicroservice/repositories/DocumentRepository.java
+++ document-microservice/src/main/java/com/example/documentmicroservice/repositories/DocumentRepository.java
@@ -9,4 +9,5 @@ import java.util.UUID;
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, UUID> {
     List<Document> findByCreatedBy(UUID userId);
+    long count();
 }
--- project-microservice/src/main/java/com/example/projectmicroservice/services/ProjectService.java
+++ project-microservice/src/main/java/com/example/projectmicroservice/services/ProjectService.java
@@ -22,4 +22,8 @@ public class ProjectService {
         projectRepository.save(project);
     }
 
+    public long countAllProjects() {
+        return projectRepository.count();
+    }
+
 }
